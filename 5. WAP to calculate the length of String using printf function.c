29ab71a6d72d22289e10c3875a7f0e81cde99f4f
DEEPAK KUMAR <dksinghdure@outlook.com>
2022-07-14 23:37:27 +0530

    Update 5. WAP to calculate the length of String using printf function.c

--- 5. WAP to calculate the length of String using printf function.c
+++ 5. WAP to calculate the length of String using printf function.c
@@ -1,13 +1,8 @@
 #include<stdio.h>
 int main()
 {
-    char str[20];
-    int i,l=0;
-    printf("enter a string\n");
-    gets(str);
-    for (i=0;i<str[i];i++)
-    {
-        l++;
-    }
-    printf("Length of this string = %d",l);
+    int d;
+    d=printf("Deepak");
+    printf("Length of this string = %d",d);
+    return 0;
 }
