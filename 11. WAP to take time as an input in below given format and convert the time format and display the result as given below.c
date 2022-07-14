35095e1a00dace5461f8686f8dd77478918fe02a
DEEPAK KUMAR <dksinghdure@outlook.com>
2022-07-14 23:42:05 +0530

    Update 11. WAP to take time as an input in below given format and convert the time format and display the result as given below.c

--- 11. WAP to take time as an input in below given format and convert the time format and display the result as given below.c
+++ 11. WAP to take time as an input in below given format and convert the time format and display the result as given below.c
@@ -3,7 +3,7 @@ int main()
 {
     int x, y;
     printf("Enter hour and minutes\nHH:MM\n");
-    scanf("%d%d", &x, &y);
+    scanf("%d:%d", &x, &y);
     printf("%d Hour and %d Minutes", x, y);
     return 0;
 }
