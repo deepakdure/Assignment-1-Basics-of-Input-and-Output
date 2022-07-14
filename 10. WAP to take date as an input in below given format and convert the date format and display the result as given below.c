1b22a299f7a55625aaaefdc0db358140ef1ec8c4
DEEPAK KUMAR <dksinghdure@outlook.com>
2022-07-14 23:41:02 +0530

    Update 10. WAP to take date as an input in below given format and convert the date format and display the result as given below.c

--- 10. WAP to take date as an input in below given format and convert the date format and display the result as given below.c
+++ 10. WAP to take date as an input in below given format and convert the date format and display the result as given below.c
@@ -3,8 +3,7 @@ int main()
 {
     int d, m, y;
     printf("DD = Date, MM = Month, YYYY = Year\n");
-    scanf("%d", &d);
-    scanf("%d", &m);
-    scanf("%d", &y);
+    scanf("%d/%d/%d", &d,&m,&y);
     printf("Day _ %d, Month _ %d, Year _ %d", d, m, y);
+    return 0;
 }
